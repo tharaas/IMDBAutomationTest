35ab97e56501ec2cfe852be415a54323c1bb9cc7
tharaas <tharaaa51@gmail.com>
2024-03-03 21:22:29 +0200

    done
    done

--- imdbProject/Infra/base_page.py
+++ imdbProject/Infra/base_page.py
@@ -1,10 +1,13 @@
+from selenium import webdriver
+
 
 class BasePage:
+    HUB_URL = 'http://172.20.10.3:4444'
 
     def __init__(self, driver):
-        self.driver = driver
+        self._driver = driver
         print("start test: ")
 
     def get_url_driver(self):
-        return self.driver.current_url
+        return self._driver.current_url
 
